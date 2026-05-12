4799cb039c8c8b90e68b879f7d9b63f77097bc26
Sasanka Geetanath Gorthi <sasankageetanathgorthi@Gorthis-MacBook-Pro.local>
2026-05-12 18:44:31 -0500

    Added SQL SELECT practice exercises

--- Select Statements.sql
+++ Select Statements.sql
@@ -1,50 +1,115 @@
+-- =========================================================
+-- SQL PRACTICE - SELECT STATEMENTS
+-- =========================================================
+
+
+-- =========================================================
+-- QUESTION 1
 -- Show all databases available in MySQL
+-- =========================================================
+
 SHOW DATABASES;
 
+
+-- =========================================================
+-- QUESTION 2
 -- Select the Parks_and_Recreation database to work in
+-- =========================================================
+
 USE Parks_and_Recreation;
 
+
+-- =========================================================
+-- QUESTION 3
 -- Display all columns and rows from the employee_salary table
+-- =========================================================
+
 SELECT * 
 FROM employee_salary;
 
 
--- Display only first_name and age from employee_demographics
+-- =========================================================
+-- QUESTION 4
+-- Display only:
+-- first_name
+-- age
+--
+-- From:
+-- employee_demographics
+-- =========================================================
+
 SELECT first_name, 
 age 
 FROM employee_demographics;
 
 
--- Display last_name, salary, and salary multiplied by 2
--- AS is used to create an alias called new_sal
+-- =========================================================
+-- QUESTION 5
+-- Display:
+-- last_name
+-- salary
+-- salary multiplied by 2
+--
+-- Use an alias called new_sal
+-- =========================================================
+
 SELECT last_name, 
 salary, 
 salary * 2 AS new_sal
 FROM employee_salary;
 
 
--- Display only unique department IDs from employee_salary
+-- =========================================================
+-- QUESTION 6
+-- Display only unique department IDs
+-- from employee_salary
+--
 -- DISTINCT removes duplicate values
+-- =========================================================
+
 SELECT DISTINCT department_id
 FROM employee_salary;
 
 
--- Display first_name, salary, and a calculated column
+-- =========================================================
+-- QUESTION 7
+-- Display:
+-- first_name
+-- salary
+-- a calculated column:
+-- (salary + 100) * 10
+--
 -- Parentheses run first because of PEMDAS/BODMAS
+-- =========================================================
+
 SELECT first_name,
 salary,
 (salary + 100) * 10
 FROM employee_salary;
 
 
+-- =========================================================
+-- QUESTION 8
 -- Display all columns from employee_demographics
+--
 -- * means select everything
+-- =========================================================
+
 SELECT * 
 FROM employee_demographics;
  
 
--- Display first_name, last_name, salary,
--- and salary after adding 500
+-- =========================================================
+-- QUESTION 9
+-- Display:
+-- first_name
+-- last_name
+-- salary
+-- salary after adding 500
+--
+-- Use an alias called new_sal
+-- =========================================================
+
 SELECT first_name,
 last_name,
 salary,
@@ -52,16 +117,35 @@ salary + 500 AS new_sal
 FROM employee_salary;
 
 
--- Multiplication happens before addition here
+-- =========================================================
+-- QUESTION 10
+-- Display:
+-- first_name
+-- salary
+-- a calculated column:
+-- salary + 100 * 10
+--
+-- Multiplication happens before addition
 -- because there are no parentheses
+-- =========================================================
+
 SELECT first_name,
 salary,
 salary + 100 * 10 AS new_sal
 FROM employee_salary;
 
 
--- Display first_name, last_name, salary,
--- and salary multiplied by 5
+-- =========================================================
+-- QUESTION 11
+-- Display:
+-- first_name
+-- last_name
+-- salary
+-- salary multiplied by 5
+--
+-- Use an alias called bonus_salary
+-- =========================================================
+
 SELECT first_name,
 last_name,
 salary,
@@ -69,9 +153,18 @@ salary * 5 AS bonus_salary
 FROM employee_salary;
 
 
--- Display first_name, last_name, salary,
--- and calculate annual_bonus
--- Formula: (salary + 200) * 12
+-- =========================================================
+-- QUESTION 12
+-- Display:
+-- first_name
+-- last_name
+-- salary
+-- annual_bonus
+--
+-- Formula:
+-- (salary + 200) * 12
+-- =========================================================
+
 SELECT first_name,
 last_name,
 salary,
