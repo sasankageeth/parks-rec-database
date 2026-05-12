c196c0b0385ee17bb774d6714b3e348a716750d9
Sasanka Geetanath Gorthi <sasankageetanathgorthi@Gorthis-MacBook-Pro.local>
2026-05-12 01:19:38 -0500

    Added SQL WHERE practice queries with Comments

--- WHERE Statements.sql
+++ WHERE Statements.sql
@@ -1,44 +1,70 @@
 -- Show all databases available in MySQL
 SHOW DATABASES;
 
+
 -- Select the Parks_and_Recreation database to work in
 USE Parks_and_Recreation;
 
--- Write a query to display all employees from employee_salary where salary is greater than 60000
+
+-- Display all employees from employee_salary
+-- where salary is greater than 60000
 SELECT * 
 FROM employee_salary
 WHERE salary > 60000;
 
--- Write a query to display all rows from employee_demographics where gender is NOT 'Male'
+
+-- Display all rows from employee_demographics
+-- where gender is NOT 'Male'
+-- != means not equal to
 SELECT * 
 FROM employee_demographics
 WHERE gender != 'Male';
 
--- Write a query to display all employees born after 1990-01-01 from employee_demographics
+
+-- Display all employees born after 1990-01-01
+-- from employee_demographics
 SELECT * 
 FROM employee_demographics
 WHERE birth_date > '1990-01-01';
 
--- Write a query to display employees whose first_name starts with 'J' from employee_demographics
+
+-- Display employees whose first_name starts with 'J'
+-- % means anything after J
 SELECT first_name
-FROM  employee_demographics
+FROM employee_demographics
 WHERE first_name LIKE 'J%';
 
+
+-- Display employees whose first_name:
+-- starts with 'a'
+-- and has exactly 3 characters total
+-- each _ represents one character
 SELECT *
 FROM employee_demographics
 WHERE first_name LIKE 'a__';
 
--- Write a query to display all rows from: employee_demographics where first_name starts with 'A' and has exactly 4 characters total, Use LIKE.
+
+-- Display all rows where first_name:
+-- starts with 'A'
+-- and has exactly 4 characters total
 SELECT * 
 FROM employee_demographics
 WHERE first_name LIKE 'A___';
 
--- Write a query to display all employees where: first_name starts with 'A' and has more than 5 characters From employee_demographics
+
+-- Display all employees where first_name:
+-- starts with 'A'
+-- and has more than 5 characters
+-- % means any number of characters after that
 SELECT * 
 FROM employee_demographics
 WHERE first_name LIKE 'A____%';
 
--- Write a query to display all rows from: employee_demographics where gender is 'Female' AND birth_date is after '1988-01-01'
+
+-- Display all female employees
+-- born after 1988-01-01
+-- AND is used to combine multiple conditions
 SELECT * 
 FROM employee_demographics
-WHERE gender = 'Female' AND birth_date > '1988-01-01';
+WHERE gender = 'Female' 
+AND birth_date > '1988-01-01';
